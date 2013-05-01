6e856db72ee7f3cbed6faa75836f2629c5e582d3
Joe Robles <joe@colldev.com>
2013-04-30 22:03:07 -0500

    custom 404 page

--- app/routing.php
+++ app/routing.php
@@ -27,3 +27,13 @@ $app->get('/repair', function() use($app) {
     return $app['twig']->render('templates/repair.html.twig');
 })->bind('repair');
 //end Routing
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($code == 404) {
+        $response = $app['twig']->render('templates/404.html.twig');
+        return new Response($response, 404);
+    } else {
+        $response = $app['twig']->render('templates/error.html.twig', array('code' => $code));
+        return new Response($response, $code);
+    }
+});
--- web/app.php
+++ web/app.php
@@ -7,16 +7,6 @@ $app = new Application();
 require_once __DIR__ . '/../app/bootstrap.php';
 require_once __DIR__ . '/../app/routing.php';
 
-$app->error(function (\Exception $e, $code) use ($app) {
-    if ($code == 404) {
-        $response = $app['twig']->render('templates/404.html.twig');
-        return new Response($response, 404);
-    } else {
-        $response = $app['twig']->render('templates/error.html.twig', array('code' => $code));
-        return new Response($response, $code);
-    }
-});
-
 if ($app['debug']) {
     $app->run();
 } else {
